16299b35ff25dab4d40f1b0320312d8d85502e59
SANTIAGO ALEJANDRO PALMA <47490026@ort>
2024-06-10 12:00:31 -0300

    creacion rutas

---
+++ src/app/casos/page.js
@@ -0,0 +1,9 @@
+export default function Casos() {
+    return (
+      <main >
+        <div>
+        <h1>aisjoasijdiod</h1>
+        </div>
+      </main>
+    );
+  }
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+      <main >
+        <div>
+        <h1>aisjoasijdiod</h1>
+        </div>
+      </main>
+    );
+  }
